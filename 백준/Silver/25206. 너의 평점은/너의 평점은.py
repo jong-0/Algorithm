ac7aa5f51b3db84e22b6d9a572ccf6faecdfeddd
jong-0 <111297101+jong-0@users.noreply.github.com>
2023-04-12 22:27:27 +0900

    [Silver V] Title: 너의 평점은, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+dict = {'A+' : 4.5, 'A0' : 4.0, 'B+' : 3.5, 'B0' : 3.0, 'C+' : 2.5, 'C0' : 2.0, 'D+' : 1.5, 'D0' : 1.0, 'F' : 0.0}
+
+credit = 0
+total = 0
+
+for i in range(20):
+  a, c, g = input().split()
+  if g == 'P':
+    continue
+  else:
+    credit += float(c)
+    total += float(c) * dict[g]
+
+print(total/credit)
