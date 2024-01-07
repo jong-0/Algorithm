480b8c347e942597c31a7f6e9108ce12e2ddfd2b
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-07 15:22:47 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,13 @@
-num = 0
-s = 0
+max = 0
+idx = 0
+cnt = 0
+
 for i in range(1,10):
     n = int(input())
-    if n > s:
-        s = n
-        num = i
+    cnt += 1
+    if n > max:
+        max = n
+        idx = cnt
 
-print(s)
-print(num)
+print(max)
+print(idx)
