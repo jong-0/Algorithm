df7bd2ea24816a6578e304d43577a4b130f2a100
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-08 20:33:21 +0900

    [Bronze II] Title: 수 정렬하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,12 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 num = int(input())
-li = [0]*num
+li=[0]*num
 
 for i in range(num):
     li[i] = int(input())
 
 li.sort()
 
-print(*li, sep='\n')
+for i in li:
+    print(i)
