b12cf689bea5f791dd458ddbf7a4734576c4563e
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-10 01:35:33 +0900

    [Bronze V] Title: 개수 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,14 +1,11 @@
-import sys
-input = sys.stdin.readline
-
-num = int(input())
-li = list(map(int, input().split()))
 n = int(input())
+li = list(map(int, input().split()))
+chk = int(input())
 
-s = 0
+ans = 0
 
 for i in li:
-    if n == i:
-        s+=1
+    if i == chk:
+        ans += 1
 
-print(s)
+print(ans)
