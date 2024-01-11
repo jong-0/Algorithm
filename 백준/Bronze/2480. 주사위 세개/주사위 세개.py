7914de9e3533b5260dafde01e303afe72b81c0e3
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-11 17:13:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,9 @@
 li = list(map(int, input().split()))
-
 li.sort()
 
-if li[0] == li[2]:
-    print(10000 + li[0]*1000)
-elif li[1] == li[0] or li[1] == li[2]:
+if li[0] == li[1] == li[2]:
+    print(li[0]*1000 + 10000)
+elif li[0] == li[1] or li[1] == li[2]:
     print(1000 + li[1]*100)
 else:
     print(li[2]*100)
