10c0f254c69da85df7f661ec92b8415759df8c6d
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-18 21:35:29 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,19 +1,16 @@
-import sys
-
 li = []
-max = 0
+for _ in range(9):
+    li.append(list(map(int,input().split())))
+
 ii = 1
 jj = 1
-
-for i in range(9):
-    li.append(list(map(int, sys.stdin.readline().rstrip().split())))
-
+max = 0
 for i in range(9):
     for j in range(9):
         if li[i][j] > max:
             max = li[i][j]
-            ii = i+1
-            jj = j+1
+            ii = i + 1
+            jj = j + 1
 
 print(max)
 print(ii, jj)
