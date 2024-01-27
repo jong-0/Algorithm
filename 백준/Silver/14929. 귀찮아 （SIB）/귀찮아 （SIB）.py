bc9bd7d038b23227f8f16d11478b366ff82729fe
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-01-27 14:57:51 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 100 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
@@ -0,0 +1,16 @@
+n = int(input())
+if n <= 1:
+    print(0)
+else:
+    li = list(map(int, input().split()))
+    chk = [0] * (n-1)
+    chk[0] = li[0]
+
+    for i in range(1, len(chk)):
+        chk[i] = chk[i-1] + li[i]
+
+    sum = 0
+    for i in range(len(chk)):
+        sum += chk[i] * li[i+1]
+
+    print(sum)
