4734a3fa93ea6a76e5b3e1df0cbf2bb92c92b085
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-02-01 02:42:27 +0900

    [Silver III] Title: 수열, Time: 100 ms, Memory: 40156 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+li = [0] + list(map(int, input().rstrip().split()))
+
+chk = [0] * (n-m+1)
+
+for i in range(1,n+1):
+    li[i] = li[i-1] + li[i]
+
+for i in range(len(chk)):
+    chk[i] = li[i+m] - li[i]
+
+print(max(chk))
