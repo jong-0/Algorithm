6e77947bc690be5363370e8494da371f7bea4d78
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-02-02 22:27:38 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,41 +1,39 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-q = deque()
+dq = deque()
 n = int(input())
-
 for _ in range(n):
-    a = input().split()
-
-    if a[0] == 'back':
-        if q:
-            print(q[-1])
+    s = input().split()
+    if s[0] == 'push_back':
+        dq.append(s[-1])
+    elif s[0] == 'push_front':
+        dq.appendleft(s[-1])
+    elif s[0] == 'pop_front':
+        if len(dq) > 0:
+            print(dq.popleft())
         else:
             print(-1)
-    elif a[0] == 'front':
-        if q:
-            print(q[0])
+    elif s[0] == 'pop_back':
+        if len(dq) > 0:
+            print(dq.pop())
         else:
             print(-1)
-    elif a[0] == 'empty':
-        if q:
+    elif s[0] == 'size':
+        print(len(dq))
+    elif s[0] == 'empty':
+        if len(dq) > 0:
             print(0)
         else:
             print(1)
-    elif a[0] == 'size':
-        print(len(q))
-    elif a[0] == 'pop_back':
-        if q:
-            print(q.pop())
+    elif s[0] == 'front':
+        if len(dq) > 0:
+            print(dq[0])
         else:
             print(-1)
-    elif a[0] == 'pop_front':
-        if q:
-            print(q.popleft())
-        else:
-            print(-1)
-    elif a[0] == 'push_back':
-        q.append(a[1])
     else:
-        q.appendleft(a[1])
+        if len(dq) > 0:
+            print(dq[-1])
+        else:
+            print(-1)
