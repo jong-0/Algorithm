6c33053971bafdc8f770a741054072555507186f
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-02-02 22:24:08 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -2,33 +2,31 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-q = deque()
-
+dq = deque()
 n = int(input())
-for i in range(n):
-    a = input().split()
-
-    if a[0] == 'back':
-        if q:
-            print(q[-1])
+for _ in range(n):
+    s = input().split()
+    if s[0] == 'push':
+        dq.append(s[-1])
+    elif s[0] == 'pop':
+        if len(dq) > 0:
+            print(dq.popleft())
         else:
             print(-1)
-    elif a[0] == 'front':
-        if q:
-            print(q[0])
-        else:
-            print(-1)
-    elif a[0] == 'empty':
-        if q:
+    elif s[0] == 'size':
+        print(len(dq))
+    elif s[0] == 'empty':
+        if len(dq) > 0:
             print(0)
         else:
             print(1)
-    elif a[0] == 'size':
-        print(len(q))
-    elif a[0] == 'pop':
-        if q:
-            print(q.popleft())
+    elif s[0] == 'front':
+        if len(dq) > 0:
+            print(dq[0])
         else:
             print(-1)
     else:
-        q.append(a[1])
+        if len(dq) > 0:
+            print(dq[-1])
+        else:
+            print(-1)
