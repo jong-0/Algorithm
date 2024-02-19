35d0881ad6f352284393e223380ecd76c6dff544
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-02-19 21:19:38 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,8 @@
-abc = 'abcdefghijklmnopqrstuvwxyz'
-
 n = int(input())
 s = input()
 
 ans = 0
 for i in range(n):
-    ans += (abc.index(s[i])+1) * 31**i
+    ans += (ord(s[i])-96) * 31**i
 
-print(ans)
+print(ans % 1234567891)
