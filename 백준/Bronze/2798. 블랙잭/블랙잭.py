52a0225ad9f35af64655d6d17fd974696b6bf5bc
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-03-01 15:02:58 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,14 +2,12 @@ n, m = map(int, input().split())
 li = list(map(int, input().split()))
 
 ans = 0
+min = m
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            if abs(m-(li[i] + li[j] + li[k])) < min and (li[i] + li[j] + li[k]) <= m:
+                min = abs(m-(li[i] + li[j] + li[k]))
+                ans = (li[i] + li[j] + li[k])
 
-for i in range(n):
-    for j in range(n-2):
-        for k in range(j+1, n-1):
-            for l in range(k+1, n):
-                sum = li[j] + li[k] + li[l]
-                if sum <= m and sum > ans:
-                    ans = sum
-                else:
-                    continue
 print(ans)
