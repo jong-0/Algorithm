b0467e359adfb579694a715fe6a057b134eb3862
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-03-15 01:14:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 632 ms, Memory: 70380 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(1+n)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for i in range(1, n+1):
+    graph[i].sort()
+
+visited = [-1] * (n+1)
+
+def dfs(v, d):
+    visited[v] = d
+
+    for i in graph[v]:
+        if visited[i] == -1:
+            dfs(i, d+1)
+
+dfs(v,0)
+for i in range(1, n+1):
+    print(visited[i])
