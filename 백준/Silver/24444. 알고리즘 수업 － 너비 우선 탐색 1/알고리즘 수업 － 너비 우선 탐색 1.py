340362720fcbddb8f94389b3b98e3009c11323aa
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-03-15 14:34:06 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 552 ms, Memory: 58620 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for i in range(1, n+1):
+    graph[i].sort()
+
+visited = [0] * (n+1)
+
+cnt = 1
+def bfs(start):
+    global cnt
+    dq = deque([start])
+    visited[start] = cnt
+
+    while dq:
+        v = dq.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                dq.append(i)
+                cnt += 1
+                visited[i] = cnt
+
+bfs(v)
+for i in range(1, n+1):
+    print(visited[i])
