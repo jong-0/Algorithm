3869a909f70faa4b9f6ab7024a37512e8a606311
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-05-25 22:17:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 55700 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
 d = {}
 
 for i in range(1, n+1):
     a = input().rstrip()
     d[i] = a
     d[a] = i
-
+    
 for i in range(m):
     chk = input().rstrip()
     if chk.isalpha():
