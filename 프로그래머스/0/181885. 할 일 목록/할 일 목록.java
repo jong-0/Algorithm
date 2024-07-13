487271d0a417997091bae3063c8c0ca7fb8efb10
jong-0 <111297101+jong-0@users.noreply.github.com>
2024-07-14 00:02:58 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                list.add(todo_list[i]);
+            }
+        }
+        
+        String[] answer = {};
+        answer = list.toArray(answer);
+        return answer;
+    }
+}
